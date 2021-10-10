56a4f63a4d6136bab5e9217439ef2c5b8d522932
Beggiice <beggiice@gmail.com>
2021-10-10 21:12:54 +0000

    Uppdeit 2

--- NTV/Assignment 3.3.dart
+++ NTV/Assignment 3.3.dart
@@ -1,4 +1,9 @@
+/*
+Write a program to read 5 numbers from keyboard, using a loop.
+The program should work with any 5 numbers. Program ends by printing out the
+sum and average of the numbers
 
+*/
 
 
 
--- bin/project.dart
+++ bin/project.dart
@@ -2,4 +2,6 @@ import 'package:project/project.dart' as project;
 
 void main(List<String> arguments) {
   print('Hello world: ${project.calculate()}!');
+
 }
+
